9d37a5fb97e86e15c07de9616d478105e5212d82
mmd1380 <mamadreza460@gmail.com>
2020-11-16 14:10:15 +0330

    .

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">Abou54t</router-link>
+      <router-link to="/about">Abou54t</router-link>asdfghjkjhgfdsa
     </div>
     <router-view/>
   </div>
